c7facd21dc7bb51267628141e6dad6ff0db101be
LenGaL <pol.mansart@gmail.com>
2019-11-20 21:13:33 +0100

    added 3 new commands, 2 for roles, 1 to purge msg

--- index.js
+++ index.js
@@ -9,10 +9,14 @@ bot.on('ready', function () {
 bot.login(process.env.TOKEN_KEY);
 
 let users = [];
+let arrayRoles = [];
+
 
 bot.on('message', message => {
   let isBot = message.author.bot == true;
-  if (message.content === '!lgcreate' && users.length === 0 && !isBot) {
+  let con = message.content;
+
+  if (con === '!lgcreate' && users.length === 0 && !isBot) {
     message.channel.send('Vous avez créer une partie de Loup-Garou.');
     users.push(
       {
@@ -24,17 +28,18 @@ bot.on('message', message => {
     )
     console.log(users);
     //console.log(users.length);
-  } else if (message.content === '!lgcreate' && users.length > 0 && !isBot){
+  } else if (con === '!lgcreate' && users.length > 0 && !isBot){
     message.channel.send('Une partie de Loup-Garou est déjà en préparation.');
-  } else if (message.content === '!lgjoin' && users.length >= 1 && !isBot) {
+  } else if (con === '!lgjoin' && users.length >= 1 && !isBot) {
     for (i = 0; i < users.length; i++) {
-      if (!users.find( user=> user.id === message.author.id)){
+      if (!users.find( user => user.id === message.author.id)){
         users.push(
           {
           id: message.author.id,
           name: message.author.username,
           status: 0,
-          role: ""
+          role: "",
+          votes: 0
           }
         )
         message.channel.send(`Vous êtes ${users.length} dans la partie.`);
@@ -46,18 +51,66 @@ bot.on('message', message => {
       }
     }
   } else if (isBot) {
+
     console.log(message.author.bot);
-  } else if (!isBot) {
-    message.channel.send('oui bah nan');
-    message.member.setRoles()
-    .then(console.log)
-    .catch(console.error);
-   message.guild.createRole({name: '⠀',
-   color: 'BLUE',
- })
-   .then(role => console.log(`Created new role with name ${role.name} and color ${role.color}`))
-   .catch(console.error);
-    //message.member.addRole({name: '⠀'});
-    //message.guild.fetchMembers()
+
+  } else if (con === '!lgroles') {
+
+    function returnRole (role) {
+      arrayRoles.push(role);
+      //console.log(`Created new role with name ${role.name} and color ${role.color}`);
+    };
+   
+
+    (async () => {
+      await  message.guild.createRole({name: '⠀', color: 'RED',})
+      .then(role => returnRole(role))
+      .catch(console.error);
+      await message.guild.createRole({name: '⠀', color: 'GREEN',})
+      .then(role => returnRole(role))
+      .catch(console.error);
+      await message.guild.createRole({name: '⠀', color: 'BLUE',})
+      .then (role => returnRole(role))
+      .catch(console.error);
+
+      console.log(arrayRoles);
+      //console.log(arrayRoles[0]);
+    })();
+
+  } else if (con === '!lgdelete'){
+
+      for (let i = 0; i < arrayRoles.length; i++){
+          message.guild.roles.find(role => role.id === arrayRoles[i].id).delete().then( () => {
+            for (let i = 0; i < arrayRoles.length; i++){
+              if (arrayRoles.find( role => role.deleted=== true)) {
+                arrayRoles.splice(i, 1);
+                i--;
+              }
+            };
+          });
+          console.log(`deleted ${arrayRoles[i].id} successfully`);
+      }
+      setTimeout(() => {
+        console.log(arrayRoles);  
+      }, 1000);
+
+  } else if (con === '!purge'){
+    console.clear();
+    let way = message.content.slice(6).split(' ');
+    let x = way.slice(0);
+    
+    async function purge() {
+      
+      const messages = await message.channel.fetchMessages({limit: x});
+      channel.bulkDelete(messages)
+      .then(messages => console.log(`Deleted ${messages.size} messages`))
+      .catch(console.error);
+      
+
+    }
+    purge();
+
+    console.log('Clearing messages');
   }
+
 });
