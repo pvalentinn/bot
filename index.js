7e507980793ec9844e1b13616c8a3164d7c5729d
LenGaL <pol.mansart@gmail.com>
2019-11-21 01:12:41 +0100

    fixed and finished !purge command, roles command laggy

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ bot.login(process.env.TOKEN_KEY);
 let users = [];
 let arrayRoles = [];
 
-
 bot.on('message', message => {
   let isBot = message.author.bot == true;
   let con = message.content;
@@ -61,56 +60,60 @@ bot.on('message', message => {
       //console.log(`Created new role with name ${role.name} and color ${role.color}`);
     };
    
-
-    (async () => {
+     (async () => {
       await  message.guild.createRole({name: '⠀', color: 'RED',})
-      .then(role => returnRole(role))
-      .catch(console.error);
-      await message.guild.createRole({name: '⠀', color: 'GREEN',})
-      .then(role => returnRole(role))
-      .catch(console.error);
-      await message.guild.createRole({name: '⠀', color: 'BLUE',})
-      .then (role => returnRole(role))
-      .catch(console.error);
-
-      console.log(arrayRoles);
-      //console.log(arrayRoles[0]);
-    })();
+		  .then(role => returnRole(role))
+		  .catch(console.error);
+		  await message.guild.createRole({name: '⠀', color: 'GREEN',})
+		  .then(role => returnRole(role))
+		  .catch(console.error);
+		  await message.guild.createRole({name: '⠀', color: 'BLUE',})
+		  .then (role => returnRole(role))
+		  .catch(console.error);
+	
+		  console.log(arrayRoles);
+		  //console.log(arrayRoles[0]);
+     })();
 
   } else if (con === '!lgdelete'){
 
-      for (let i = 0; i < arrayRoles.length; i++){
-          message.guild.roles.find(role => role.id === arrayRoles[i].id).delete().then( () => {
-            for (let i = 0; i < arrayRoles.length; i++){
-              if (arrayRoles.find( role => role.deleted=== true)) {
-                arrayRoles.splice(i, 1);
-                i--;
-              }
-            };
-          });
-          console.log(`deleted ${arrayRoles[i].id} successfully`);
-      }
-      setTimeout(() => {
-        console.log(arrayRoles);  
-      }, 1000);
+    for (let i = 0; i < arrayRoles.length; i++){
+      message.guild.roles.find(role => role.id === arrayRoles[i].id).delete().then( () => {
+        for (let i = 0; i < arrayRoles.length; i++){
+          if (arrayRoles.find( role => role.deleted=== true)) {
+            arrayRoles.splice(i, 1);
+            i--;
+          }
+        };
+      });
+      console.log(`deleted ${arrayRoles[i].id} successfully`);
+  }
+  setTimeout(() => {
+    console.log(arrayRoles);  
+  }, 1000);
 
-  } else if (con === '!purge'){
+  
+  } else if (message.content.startsWith('!purge')){
     console.clear();
     let way = message.content.slice(6).split(' ');
-    let x = way.slice(0);
-    
-    async function purge() {
-      
-      const messages = await message.channel.fetchMessages({limit: x});
-      channel.bulkDelete(messages)
-      .then(messages => console.log(`Deleted ${messages.size} messages`))
-      .catch(console.error);
-      
+    let xay = way.slice(1);    
 
+    if ( isNaN(xay) === true || xay.length > 1  || Number(xay) === NaN ) message.channel.send("La commande nécessite '!purge' suivi d'un nombre.");
+    if ( xay > 100 || xay == 0) message.channel.send("La commande n'accepete que les nombres au dessus 0 et en dessous 101.");
+    else {
+      async function purge() {
+      
+        const messages =  await message.channel.fetchMessages({limit: xay});
+        message.channel.bulkDelete(messages)
+        .then(messages => console.log(`Deleted ${messages.size} messages`))
+        .catch(console.error);
+        message.channel.send(`Deleted ${messages.size} messages`);
+  
+      }
+      purge();
+  
+      console.log('Clearing messages');
     }
-    purge();
-
-    console.log('Clearing messages');
+     
   }
-
 });
